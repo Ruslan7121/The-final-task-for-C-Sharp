02195224623dfc3cb6452e4d7b8a390918571534
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-09-03 12:10:12 +0300

    Program code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array_1 = new string[8] { "65", "cat", "mouse", "dog", "777", "fly", "book", "lamp" };
+string[] array_2 = new string[array_1.Length];
+
+void ThreeLineElements(string[] array_1, string[] array_2)
+{
+    int count = 0;
+    for (int i = 0; i < array_1.Length; i++)
+    {
+        if (array_1[i].Length <= 3)
+        {
+            array_2[count] = array_1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+ThreeLineElements(array_1, array_2);
+PrintArray(array_2);
